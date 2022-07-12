e7e1457fb450ca5bb0cabbc2fcd9b2607377dd81
aroughcun <104947406+aroughcun@users.noreply.github.com>
2022-07-11 21:51:02 -0400

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -9,11 +9,8 @@ fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
-# Reset tables for each new run of script
-# $($PQSL "TRUNCATE TABLE teams, games;")
-
 # Read file line by line
-cat games_test.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS 
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS 
 do
 if [[ $YEAR != 'year' ]] # Check for header line and only loop over non-header lines  
 then
@@ -34,14 +31,21 @@ then
     OPPONENT_INSERT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT');")
     echo Inserted opponent team: $OPPONENT
   fi
-  
-# Games table 
 
-# Add year to the year column
-# Add round to the round column
-# Get team_id from teams table corresponding to winning team name
-# Get team_id from teams table corresponding to opponent team name
-# Get winner goals from winner_goals column 
-# Get opponent goals from opponent_goals column
+  # Get team_id from teams table corresponding to winner/opponent
+  WIN_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+  OPP_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+  
+  # Insert games data
+  GAMES_INSERT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WIN_ID, $OPP_ID, $WINNER_GOALS, $OPPONENT_GOALS);")
+  echo Inserted year: $YEAR, round: $ROUND, winner_id: $WIN_ID, opponent_id: $OPP_ID, winner_goals: $WINNER_GOALS, opponent_goals: $OPPONENT_GOALS
+  
+  # Insert winner_id and opponent_id into games table
+  #TEAM_ID_INSERT=$($PSQL "INSERT INTO games(winner_id, opponent_id) VALUES($WIN_ID, $OPP_ID);")
+  #echo Inserted winner_id: $WIN_ID and opponent_id: $OPP_ID
+
+  # Insert goals for winners and opponents
+  #GOALS_INSERT=$($PSQL "INSERT INTO games(winner_goals, opponent_goals) VALUES($WINNER_GOALS, $OPPONENT_GOALS);")
+  #echo Inserted winner goals: $WINNER_GOALS and opponent goals: $OPPONENT_GOALS
 fi
 done
