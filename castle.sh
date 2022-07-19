35fa9ba93564fffd97c02ba0612fe33fc775223a
aroughcun <104947406+aroughcun@users.noreply.github.com>
2022-07-19 11:59:16 -0400

    Create castle.sh

---
+++ castle.sh
@@ -0,0 +1,24 @@
+echo "
+Welcome to my castle
+"
+echo "
+ /\                /\ 
+/  \              /  \ 
+|  |______________|  |
+|                    |
+|   []    []    []   |
+|                    |
+|         __         |
+|________|  |________|
+"
+# Created with nano, which allows file editing within the terminal
+
+# ^ character in front of command --> <CTRL>
+# M character in front of command --> "Meta" - a key that doesn't exist on most keyboards, therefore if on OSX use <escape>, for all others use <ALT>
+
+# to open file: nano <filename>
+# to save file: <CTRL> + o (gives option to edit/rename file, otherwise <ENTER> to save)
+# to exit nano: <CTRL> + x
+
+# <CTRL> + k: cut entire line
+# <CTRL> + u: uncut entire line (can be used repeatedly in a "copy & paste" fashion)
